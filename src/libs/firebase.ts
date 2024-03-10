c798b2f540953c27c3ebf49a046f39b117901843
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-03-10 17:00:40 -0300

    inital commit

---
+++ src/libs/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from 'firebase/storage'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCsQmIVO_PS3IaDNXt4sNBZ2cPVowMXSDA",
+    authDomain: "gallery-c1598.firebaseapp.com",
+    projectId: "gallery-c1598",
+    storageBucket: "gallery-c1598.appspot.com",
+    messagingSenderId: "257061931459",
+    appId: "1:257061931459:web:e83d364b93d8ba70a1d576"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const storage = getStorage(firebaseApp)
